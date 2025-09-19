4707c3b23de2d2b45d3329d38eba72bc412bf10b
Aayurcare2025 <info@aayurcare.com>
2025-09-19 23:16:39 +0530

    push code

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   // const app = await NestFactory.create(AppModule);
     const app = await NestFactory.create(AppModule,{cors:true});
- await app.listen(5000);
+//  await app.listen(5000);
+ await app.listen(5000, '0.0.0.0');
+ 
 }
 
 bootstrap();
