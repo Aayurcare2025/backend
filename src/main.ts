e08da28f46963c2f2699de1ecbb1a9e5f2a89643
Aayurcare2025 <info@aayurcare.com>
2025-09-20 19:08:44 +0530

    push code

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ async function bootstrap() {
   // const app = await NestFactory.create(AppModule);
     const app = await NestFactory.create(AppModule,{cors:true});
 //  await app.listen(5000);
+ app.enableCors({
+    origin: 'https://www.aayurcare.com',  // frontend domain
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
  await app.listen(5000, '0.0.0.0');
  
 }
