608ef814e92fd61bbcf01294a12246ce1aa2cf76
info@aayurcare.com <info@aayurcare.com>
2025-10-01 09:52:06 +0530

    COMMIT

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as bodyParser from 'body-parser'
 async function bootstrap() {
   // const app = await NestFactory.create(AppModule);
     const app = await NestFactory.create(AppModule,{cors:true});
@@ -10,6 +10,11 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
+
+
+
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
  await app.listen(5000, '0.0.0.0');
  
 }
